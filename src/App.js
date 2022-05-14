c9ed2d6036389bb0bcbefb21ebaf9b493342b428
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-05-14 17:22:20 +0300

    update public route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Services from "./pages/Services";
+import { publicRoute } from "./routes/publicRoute";
 
 function App() {
   return (
     <div >
       <Navbar >
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
+          {
+            publicRoute.map(({ path, Component }, index) => <Route key={index} path={path} element={<Component />} />)
+          }
         </Routes>
       </Navbar>
     </div>
